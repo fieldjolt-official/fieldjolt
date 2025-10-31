61b4abb268d8a3d580755797f702c8a6c3b49756
Morgan Stovold <morgannstovold.dev@gmail.com>
2025-10-30 23:33:56 -0700

    feat: add Suspense wrapper to root providers

--- apps/app/src/components/providers.tsx
+++ apps/app/src/components/providers.tsx
@@ -4,23 +4,26 @@ import { ReactQueryDevtools, TRPCReactProvider } from "@fieldjolt/api/react";
 import { Toaster } from "@fieldjolt/ui/components/sonner";
 import { ThemeProvider } from "next-themes";
 import { NuqsAdapter } from "nuqs/adapters/react";
+import { Suspense } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NuqsAdapter>
-      <TRPCReactProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          disableTransitionOnChange
-          enableColorScheme
-          enableSystem
-        >
-          {children}
-          <Toaster richColors />
-        </ThemeProvider>
-        <ReactQueryDevtools />
-      </TRPCReactProvider>
-    </NuqsAdapter>
+    <Suspense>
+      <NuqsAdapter>
+        <TRPCReactProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            disableTransitionOnChange
+            enableColorScheme
+            enableSystem
+          >
+            {children}
+            <Toaster richColors />
+          </ThemeProvider>
+          <ReactQueryDevtools />
+        </TRPCReactProvider>
+      </NuqsAdapter>
+    </Suspense>
   );
 }
